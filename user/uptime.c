f947866111ab4f6f5919b44e32fc3c7f9feee1be
MotingLiuu <moting.liuu@gmail.com>
2026-09-10 09:07:11 +0800

    doen uptime.c

--- user/uptime.c
+++ user/uptime.c
@@ -1,8 +1,5 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
 #include "user/user.h"
-#include "kernel/fs.h"
-#include "kernel/fcntl.h"
 
 int main(void) {
   fprintf(1, "Uptime in ticks: %d\n", uptime());
