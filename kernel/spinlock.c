c66fa917ed0ade8072a41813e6e065117f84089f
MotingLiuu <moting.liuu@gmail.com>
2026-09-14 18:24:28 +0800

    done hw3

--- kernel/spinlock.c
+++ kernel/spinlock.c
@@ -31,12 +31,17 @@ acquire(struct spinlock *lk)
   //   amoswap.w.aq a5, a5, (s1)
 
   while(__sync_lock_test_and_set(&lk->locked, 1) != 0)
-    ;
+     ;
   /* code with bug
   while (*(volatile int *) &lk->locked != 0)
     ;
   lk->locked = 1;
   */
+
+  // while(lk->locked != 0)
+  //   ;
+  // lk->locked = 1;
+  //
 
   // Tell the C compiler and the processor to not move loads or stores
   // past this point, to ensure that the critical section's memory
